a88fd3f22087d29749e355cbfa9015123874e492
theabdul97@gmail.com <theabdul97@gmail.com>
2024-02-05 22:16:44 +0530

    NavLink

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,31 +1,38 @@
 import Link from "next/link";
 import Image from "next/image";
 import logoImg from "@/assets/logo.png";
-import classes from './main-header.module.css';
+import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 export default function MainHeader() {
-  console.log('Execution');
-  return ( 
-  <>
-    <MainHeaderBackground/>
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <Image src={logoImg} alt="A plate with food on it" priority />
-        NextLevel Food
-      </Link> 
+  console.log("Execution.1.1.");
 
-    <nav className={classes.nav}>
-        <ul>
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
             <li>
-                <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-                <Link href="/community">Foodie Community</Link>
+              {/* <Link
+                href="/community"
+                className="{path.startsWith('/community') ? classes.active : undefined }"
+              >
+                Foodie Community
+              </Link> */}
+              <NavLink href="/community">Foodie Community</NavLink>
             </li>
-        </ul>
-    </nav>
-
-    </header>
-    </>  
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
---
+++ components/main-header/nav-link.js
@@ -0,0 +1,16 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import classes from "./nav-link.module.css";
+
+export default function NavLink({ href, children }) {
+  const path = usePathname();
+  return (
+    <Link
+      href="/meals"
+      className="{path.startsWith('/meals') ? classes.active : undefined }"
+    >
+      Browse Meals
+    </Link>
+  );
+}
